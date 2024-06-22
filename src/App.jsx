b46cc8cf35cee917180f63fa3dcc5c8033fcf8f4
Stanley Ke <kestanley101@gmail.com>
2024-06-21 22:15:07 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("Hunter X Hunter");
   }, []);
 
   const searchMovies = async (title) => {
